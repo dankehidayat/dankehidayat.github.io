3b249f25b0a4e83e6e973e1ca93123499e4e6a4f
Danke Hidayat <dnk.hidayat@gmail.com>
2025-10-14 09:53:37 +0700

    Change the website name in rss.xml

--- src/app/rss.xml/route.ts
+++ src/app/rss.xml/route.ts
@@ -27,18 +27,18 @@ export async function GET() {
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/">
   <channel>
     <title>Danke Hidayat - Blog</title>
-    <link>https://yourdomain.com/blog</link>
+    <link>https://dankehidayat.my.id/blog</link>
     <description>Thoughts on technology, development, and more. Sharing insights from my journey in software development and research.</description>
     <language>en-us</language>
     <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
-    <atom:link href="https://yourdomain.com/rss.xml" rel="self" type="application/rss+xml" />
+    <atom:link href="https://dankehidayat.my.id/rss.xml" rel="self" type="application/rss+xml" />
     ${posts
       .map((post) => {
         const fullContent = getFullPostContent(post.slug);
         return `
     <item>
       <title>${escapeXml(post.title)}</title>
-      <link>https://yourdomain.com/blog/${post.slug}</link>
+      <link>https://dankehidayat.my.id/blog/${post.slug}</link>
       <description>${escapeXml(post.excerpt)}</description>
       <content:encoded><![CDATA[
         <!DOCTYPE html>
@@ -198,15 +198,15 @@ export async function GET() {
               </div>
               
               <footer class="read-more">
-                <p><em>Read the full post online: <a href="https://yourdomain.com/blog/${
+                <p><em>Read the full post online: <a href="https://dankehidayat.my.id/blog/${
                   post.slug
-                }">https://yourdomain.com/blog/${post.slug}</a></em></p>
+                }">https://dankehidayat.my.id/blog/${post.slug}</a></em></p>
               </footer>
             </article>
           </body>
         </html>
       ]]></content:encoded>
-      <guid>https://yourdomain.com/blog/${post.slug}</guid>
+      <guid>https://dankehidayat.my.id/blog/${post.slug}</guid>
       <pubDate>${new Date(post.date).toUTCString()}</pubDate>
       <author>${post.author}</author>
       ${post.categories
